b2502f823ed089806765a7c82210d3abd485e47d
meercurial <iwankowi495@gmail.com>
2024-06-16 23:20:31 +0300

    commit

--- src/HW/1/UserList.tsx
+++ src/HW/1/UserList.tsx
@@ -1,29 +1,31 @@
 type AddressType = {
-  street: any; // ПОДПРАВЛЯЕМ any
-  city: any; // ПОДПРАВЛЯЕМ any
+  street: string;
+  city: string;
 };
 
 type UserType = {
-  // ПРИДЕТСЯ САМОМУ)
+  id: number;
+  name: string;
+  age: number;
+  address: AddressType;
 };
 
-type UserListPropsType = {
-  users: UserType; // ПО МОЕМУ ЧЕГО-ТО НЕ ХВАТАЕТ...
-};
+interface IUserListPropsType {
+  users: UserType[];
+}
 
-export const UserList = (props: 'ВСТАВЛЯЕМ ТИПИЗАЦИЮ') => {
+export const UserList: React.FC<IUserListPropsType> = ({ users }) => {
   return (
     <div id={'hw01-users'}>
       <h2>User List:</h2>
-
-      {/*<ul>*/}
-      {/*  {props.users.map((user) => ( // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...*/}
-      {/*    <li key={ЭЛЕМЕНТ МАССИВА.id} id={`hw01-user-${ЭЛЕМЕНТ МАССИВА.id}`}>*/}
-      {/*      <strong>{ЭЛЕМЕНТ МАССИВА.name}</strong> (Age: {ЭЛЕМЕНТ МАССИВА.age})<strong> Address:</strong>*/}
-      {/*      {ЭЛЕМЕНТ МАССИВА.address.street}, {ЭЛЕМЕНТ МАССИВА.address.city}*/}
-      {/*    </li>*/}
-      {/*  ))}*/}
-      {/*</ul>*/}
+      <ul>
+        {users.map((obj) => (
+          <li key={obj.id} id={`hw01-user-${obj.id}`}>
+            <strong>{obj.name}</strong> (Age: {obj.age})<strong> Address:</strong>
+            {obj.address.street}, {obj.address.city}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
